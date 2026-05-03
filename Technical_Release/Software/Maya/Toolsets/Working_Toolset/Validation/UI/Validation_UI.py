676e965d2863c881f5c4b12c044582ca204fb18d
HuangFei <trongphi.hoang2511@gmail.com>
2026-05-03 21:41:42 +0700

    Fix Issue

--- Technical_Release/Software/Maya/Toolsets/Working_Toolset/Validation/UI/Validation_UI.py
+++ Technical_Release/Software/Maya/Toolsets/Working_Toolset/Validation/UI/Validation_UI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Validation_UI.ui'
+## Form generated from reading UI file 'Validation_UI.py.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.1
 ##
